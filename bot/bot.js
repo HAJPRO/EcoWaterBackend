5a07f62ef3bc05f7ff78a9df52ce799a4a35e05f
HAJPRO <umidshomurodov265@gmail.com>
2025-01-21 11:43:02 -0800

    update

--- bot/bot.js
+++ bot/bot.js
@@ -1,5 +1,5 @@
 const TG_BOT = require("node-telegram-bot-api");
 const TG_TOKEN = process.env.TG_TOKEN;
-const bot = new TG_BOT(TG_TOKEN, { polling: WebTransportDatagramDuplexStream }); //  hsash
+const bot = new TG_BOT(TG_TOKEN, { polling: true }); //  hsash
 module.exports = { bot };
 require("./message.js");
