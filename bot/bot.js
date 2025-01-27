31521341b33fcf95fbb167bd0a80fe5a72be2d53
Umid Shomurodov <umidshomurodov265@gmail.com>
2025-01-27 22:16:10 +0500

    update

--- bot/bot.js
+++ bot/bot.js
@@ -1,5 +1,5 @@
 const TG_BOT = require("node-telegram-bot-api");
 const TG_TOKEN = process.env.TG_TOKEN;
-const bot = new TG_BOT(TG_TOKEN, { polling: true }); //
+const bot = new TG_BOT(TG_TOKEN, { polling: false }); //
 module.exports = { bot };
 require("./message.js");
