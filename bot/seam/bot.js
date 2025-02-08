b7330da98981c3c3b53fb7e8c0d5925f052b6515
HAJPRO <umidshomurodov265@gmail.com>
2025-02-08 08:39:55 -0800

    update

--- bot/seam/bot.js
+++ bot/seam/bot.js
@@ -1,6 +1,6 @@
 const TG_BOT = require("node-telegram-bot-api");
 const TG_TOKEN_SEAM = process.env.TG_TOKEN_SEAM;
-const bot = new TG_BOT(TG_TOKEN_SEAM, { polling: true });
+const bot = new TG_BOT(TG_TOKEN_SEAM, { polling: true }); ///
 
 module.exports = { bot };
 require("../seam/message");
