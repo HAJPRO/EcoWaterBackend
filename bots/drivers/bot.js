c189dec3e990b02ba758d429324c18a2647897bf
HAJPRO <texhaj7@gmail.com>
2025-05-13 08:56:44 -0800

    Yangi o‘zgarish

--- bots/drivers/bot.js
+++ bots/drivers/bot.js
@@ -1,6 +1,6 @@
 const TG_BOT = require("node-telegram-bot-api");
 const TG_TOKEN_ORDER = process.env.TG_TOKEN_ORDER;
-const bot = new TG_BOT(TG_TOKEN_ORDER, { polling: true }); /////
+const bot = new TG_BOT(TG_TOKEN_ORDER, { polling: true }); ///////////
 
 module.exports = { bot };
 require("../drivers/message");
