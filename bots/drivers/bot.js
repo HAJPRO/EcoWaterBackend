805ba550ed74fed40c97a607a64417b321a223b1
HAJPRO <texhaj7@gmail.com>
2025-05-19 15:09:15 -0800

    Yangi o‘zgarish

--- bots/drivers/bot.js
+++ bots/drivers/bot.js
@@ -1,8 +1,23 @@
+// bot.js
+require("dotenv").config(); // Eslatma: Faylning eng boshida bo‘lsin
+
 const TG_BOT = require("node-telegram-bot-api");
 const TG_TOKEN_ORDER = process.env.TG_TOKEN_ORDER;
+
+if (!TG_TOKEN_ORDER) {
+    console.error("❌ TG_TOKEN_ORDER aniqlanmadi! .env faylni tekshiring.");
+    process.exit(1);
+}
+
 const bot = new TG_BOT(TG_TOKEN_ORDER, { polling: true });
 
-module.exports = { bot };
-require("../drivers/message");
+// Polling error handler
+bot.on("polling_error", (error) => {
+    console.error("Polling error:", error.code, error.message);
+});
 
+module.exports = { bot };
+
+// Listener fayllarni yuklash
+require("../drivers/message");
 require("../drivers/query");
